1f0f3087c0db95d66760de9924c48482642abad5
Mohammad Yeghaneh Abkenar <Mohammad.YeghanehAbkenar@bdr.de>
2025-07-23 19:05:47 +0200

    solve depdency, troubshotting notebook

--- src/expandnrc_adapter.py
+++ src/expandnrc_adapter.py
@@ -5,7 +5,7 @@ import torch
 from nrclex import NRCLex
 from tqdm import tqdm
 
-from ExpandNRC.emotion_frequences import EmotionFrequencyCalculator
+from enrc.emotion_frequences import EmotionFrequencyCalculator
 from data_preprocess_stance import preprocess_data
 from train_stance import train_model
 
@@ -47,30 +47,10 @@ def preprocess_with_expandnrc(df: pd.DataFrame, lexicon_path: str,
     for split, name in zip((X_tr, X_val), ('train', 'val')):
         desc = f"Computing ExpandNRC features ({name})"
         # Use a list comprehension with tqdm to show progress
-        split['expnrc_feats'] = [evf.vector(text) for text in tqdm(split['text'], desc=desc)]
+        # split['expnrc_feats'] = [evf.vector(text) for text in tqdm(split['text'], desc=desc)] 
+        split['nrc_feats'] = [evf.vector(" ".join(text) if isinstance(text, list) else text) 
+                         for text in tqdm(split['text'], desc=desc)]  #Test
     return X_tr, X_val, y_tr, y_val
 
 
-if __name__ == "__main__":
-    df = pd.read_json("../data/dfIBM_stance-v1.json")
-    print(df.columns)
-    X_tr, X_val, y_tr, y_val = preprocess_with_expandnrc(
-        df,
-        "../NRCLex/nrc_v3.json",
-        test_size=0.2,
-        random_state=42,
-        sample_level=False,
-        device='mps',
-        threshold=0.6
-    )
-    print("continue with training")
-    model, history, _ = train_model(
-        (X_tr['text'].tolist(), X_tr['topic_id'].tolist(),
-         X_tr['expnrc_feats'].tolist()),
-        y_tr,
-        (X_val['text'].tolist(), X_val['topic_id'].tolist(),
-         X_val['expnrc_feats'].tolist()),
-        y_val,
-        device="mps" if torch.cuda.is_available() else "cpu",
-        sample_level=False
-    )
+print("it is with change")
